8625d29c07c578ca20afada787a0ff495c28535d
SuzumiyaXZ <1635123039@qq.com>
2016-10-24 15:36:44 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python env
-#-*-coding:utf-8-*-
-from logo import *
-import os,sys
-sys.dont_write_bytecode = True
-from moudles import demo
-from moudles import Powershell_payload
-from moudles import wireless_payload
